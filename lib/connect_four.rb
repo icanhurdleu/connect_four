49db54ec0c54ca1cfc2fdb2601feb9b978b163be
Sam Ellis <icanhurdleu@gmail.com>
2020-07-14 19:51:20 -0400

    added initial functions to board and human_player classes

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,5 +1,10 @@
 # lib/connect_four.rb 
 
-require_relative "board"
-require_relative "human_player"
+# require_relative "board"
+# require_relative "human_player"
 
+white_circle = "\u26aa".encode('utf-8')
+black_circle = "\u26ab".encode('utf-8')
+
+puts white_circle
+puts black_circle
